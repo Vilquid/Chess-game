223020504c9f8ccc34f8bdf588c0a0921854bdcc
Mathis <mathis.de-gueydon@isen.yncrea.fr>
2022-05-18 18:41:04 +0200

    Imports GameRequestController.java

--- src/main/java/cin3/chess/controller/GameRequestController.java
+++ src/main/java/cin3/chess/controller/GameRequestController.java
@@ -1,4 +1,18 @@
 package cin3.chess.controller;
 
-public class GameRequestController {
+import cin3.chess.domain.GameRequest;
+import cin3.chess.repository.GameRequestRepository;
+import cin3.chess.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+public class GameRequestController
+{
+
 }
