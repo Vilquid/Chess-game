bdca17788fbf910511434801cc2f5e32bd90f89a
Mathis <mathis.de-gueydon@isen.yncrea.fr>
2022-05-18 19:14:19 +0200

    Fonction "welcome" dans IndexController
    Ajout de welcome(@AuthenticationPrincipal User user, final Model model)

--- src/main/java/cin3/chess/controller/IndexController.java
+++ src/main/java/cin3/chess/controller/IndexController.java
@@ -1,4 +1,55 @@
 package cin3.chess.controller;
 
-public class IndexController {
+
+@Controller
+public class IndexController()
+{
+	@Autowired
+	private FriendRequestRepository friendRequests;
+
+	@Autowired
+	private GameListRepository lastGames;
+
+	@Autowired
+	private GameRepository games;
+
+	@Autowired
+	private UserRepository users;
+
+	@Autowired
+	private FriendRequestService friendService;
+
+	@Autowired
+	private GameRequestRepository gameRequests;
+
+	@GetMapping("/")
+	public String welcome(@AuthenticationPrincipal User user, final Model model)
+	{
+		List<User> friends = friendService.getFriendUserList(user);
+		List<Game> currentGames = games.findByWhitePlayerOrBlackPlayerAndIsFinish(user, user, false);
+		currentGames.removeIf(Game::getFinish);
+
+		List<Game> lastGames = games.findByWhitePlayerOrBlackPlayerAndIsFinish(user, user, true);
+
+		User u = users.findByUsername(user.getUsername());
+		u.setPlaying(false);
+		u.setLogIn(true);
+		users.save(u);
+
+		// pause all the games
+		for (Game g: currentGames)
+		{
+			g.setPause(true);
+		}
+		games.saveAll(currentGames);
+
+		model.addAttribute("user", user);
+		model.addAttribute("friend_requests", friendRequests.findAllByReceiverAndIsAccepted(user, false));
+		model.addAttribute("friends", friends);
+		model.addAttribute("game_requests", gameRequests.findAllByReceiverAndIsAccepted(user, false));
+		model.addAttribute("pending_game_requests", gameRequests.findAllBySenderAndIsAccepted(user, false));
+		model.addAttribute("last_games", lastGames);
+		model.addAttribute("games", currentGames);
+		return "index";
+	}
 }
