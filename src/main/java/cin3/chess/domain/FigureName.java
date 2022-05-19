a3a17e0445bc0cb660bf55be23441259a4c8f6ba
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-19 17:48:33 +0200

    Figures de FigureName
    Création des figures du jeu.

--- src/main/java/cin3/chess/domain/FigureName.java
+++ src/main/java/cin3/chess/domain/FigureName.java
@@ -1,4 +1,35 @@
 package cin3.chess.domain;
 
-public class FigureName {
+public enum FigureName
+{
+	KING(0),
+	QUEEN(1),
+	ROOK(2),
+	BISHOP(3),
+	KNIGHT(4),
+	PAWN(5);
+
+
+	private final int value;
+
+	FigureName(int value)
+	{
+		this.value = value;
+	}
+
+	public static FigureName stringToFigureName(String name)
+	{
+		FigureName out = switch (name) {
+			case "rook" -> FigureName.ROOK;
+			case "king" -> FigureName.KING;
+			case "queen" -> FigureName.QUEEN;
+			case "bishop" -> FigureName.BISHOP;
+			case "pawn" -> FigureName.PAWN;
+			case "knight" -> FigureName.KNIGHT;
+			default -> null;
+		};
+
+		return out;
+	}
 }
+
