71fdbab8e7c579f7bbf68c2e3a4e5a9801ba1801
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-19 18:02:47 +0200

    Getters/setters de \FriendRequestForm.

--- src/main/java/cin3/chess/form/FriendRequestForm.java
+++ src/main/java/cin3/chess/form/FriendRequestForm.java
@@ -1,4 +1,40 @@
 package cin3.chess.form;
 
-public class FriendRequestForm {
+public class FriendRequestForm
+{
+	private Long id;
+
+	private String username;
+
+	private String sender;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getSender()
+	{
+		return sender;
+	}
+
+	public void setSender(String sender)
+	{
+		this.sender = sender;
+	}
 }
