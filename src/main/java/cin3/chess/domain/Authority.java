c745aa298eb6909102ed2a5e52988161f510b53e
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-19 15:09:26 +0200

    Ajout des attributs de Authority.java

--- src/main/java/cin3/chess/domain/Authority.java
+++ src/main/java/cin3/chess/domain/Authority.java
@@ -1,4 +1,39 @@
 package cin3.chess.domain;
 
-public class Authority {
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority implements GrantedAuthority
+{
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "authority_seq_gen")
+	@SequenceGenerator(name = "authority_seq_gen", sequenceName = "authority_id_seq")
+	private Long id;
+
+	private String authority;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	@Override
+	public String getAuthority()
+	{
+		return authority;
+	}
+
+	public void setAuthority(String authority)
+	{
+		this.authority = authority;
+	}
 }
