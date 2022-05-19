1b2db464e06434a4e6ab7c2540925098fe8df344
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-19 10:41:42 +0200

    Attribut de UserController

--- src/main/java/cin3/chess/controller/UserController.java
+++ src/main/java/cin3/chess/controller/UserController.java
@@ -1,4 +1,27 @@
 package cin3.chess.controller;
 
-public class UserController {
+import cin3.chess.form.UserForm;
+import cin3.chess.services.DbUserDetailsService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.logging.Logger;
+
+@Controller
+public class UserController
+{
+	//	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	//	private Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+	private final Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private DbUserDetailsService userService;
+
+	
 }
