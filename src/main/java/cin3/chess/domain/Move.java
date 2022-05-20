89240ffcedf88d48a1a2fa433625e079f0622f2d
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-20 13:59:55 +0200

    Attributs - getters - setters de Move.java

--- src/main/java/cin3/chess/domain/Move.java
+++ src/main/java/cin3/chess/domain/Move.java
@@ -1,4 +1,91 @@
 package cin3.chess.domain;
 
-public class Move {
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+public class Move
+{
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "moves_seq_gen")
+	@SequenceGenerator(name = "moves_seq_gen", sequenceName = "moves_id_seq")
+	private Long id;
+
+	@Column
+	private String positionEnd;
+
+	@Column
+	private String positionStart;
+
+	@Column
+	private Integer player;
+
+	@Column
+	private Long time;
+
+	@ManyToOne
+	private Game game;
+
+	public Long getTime()
+	{
+		return time;
+	}
+
+	public void setTime(Long time)
+	{
+		this.time = time;
+	}
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getPositionEnd()
+	{
+		return positionEnd;
+	}
+
+	public void setPositionEnd(String positionEnd)
+	{
+		this.positionEnd = positionEnd;
+	}
+
+	public String getPositionStart()
+	{
+		return positionStart;
+	}
+
+	public void setPositionStart(String positionStart)
+	{
+		this.positionStart = positionStart;
+	}
+
+	public Integer getPlayer()
+	{
+		return player;
+	}
+
+	public void setPlayer(Integer player)
+	{
+		this.player = player;
+	}
+
+	public Game getGame()
+	{
+		return game;
+	}
+
+	public void setGame(Game game)
+	{
+		this.game = game;
+	}
 }
+
