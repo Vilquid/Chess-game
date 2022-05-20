c66fbf9958682979607006b48986af7a8c40ed5d
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-20 15:46:38 +0200

    Attributs - getters - setters de UserForm.java

--- src/main/java/cin3/chess/form/UserForm.java
+++ src/main/java/cin3/chess/form/UserForm.java
@@ -1,4 +1,76 @@
 package cin3.chess.form;
 
-public class UserForm {
+import javax.validation.constraints.*;
+
+public class UserForm
+{
+	private Long id;
+
+	@NotEmpty
+	@Size(min = 6, max = 32)
+	private String username;
+
+	@NotEmpty
+	@Email
+	private String email;
+
+	@NotEmpty
+	@Size(min = 8, max = 30)
+	@NotBlank
+	@NotNull
+	private String password;
+
+	@NotBlank
+	private String confirmPassword;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	public String getConfirmPassword()
+	{
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword)
+	{
+		this.confirmPassword = confirmPassword;
+	}
 }
+
