01b9aa8e93869f6cc87946a97e28c502726f1a77
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-20 15:29:27 +0200

    Attributs de PromoteForm.java

--- src/main/java/cin3/chess/form/PromoteForm.java
+++ src/main/java/cin3/chess/form/PromoteForm.java
@@ -1,4 +1,31 @@
 package cin3.chess.form;
 
-public class PromoteForm {
+import javax.validation.constraints.NotEmpty;
+
+public class PromoteForm
+{
+	private Long id;
+
+	@NotEmpty
+	private String name;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
