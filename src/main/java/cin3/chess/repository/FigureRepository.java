c3d7fec3950e9abe620e58b383cf665373a072b3
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-20 16:58:08 +0200

    Création de méthodes temporaires dans FigureRepository

--- src/main/java/cin3/chess/repository/FigureRepository.java
+++ src/main/java/cin3/chess/repository/FigureRepository.java
@@ -1,4 +1,42 @@
 package cin3.chess.repository;
 
-public class FigureRepository {
+import cin3.chess.domain.Figure;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FigureRepository
+{
+	public void deleteAll()
+	{
+		
+	}
+
+	public void saveAll(List<Figure> grid) {
+	}
+
+	public Optional<Figure> findById(Long promoteId)
+	{
+		return null;
+	}
+
+	public void save(Figure figure)
+	{
+
+	}
+
+	public Figure getOne(Long pawnId)
+	{
+		return null;
+	}
+
+	/**
+	 * This function deletes the figure f2 from the list of figures.
+	 *
+	 * @param figure The figure to be deleted.
+	 */
+	public void delete(Figure figure)
+	{
+
+	}
 }
