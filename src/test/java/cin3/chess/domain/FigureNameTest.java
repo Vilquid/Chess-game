d4c31a1a0396694244769e72d8e5cf96936a1101
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-21 16:24:12 +0200

    Ajout TU FigureName

---
+++ src/test/java/cin3/chess/domain/FigureNameTest.java
@@ -0,0 +1,19 @@
+package cin3.chess.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FigureNameTest
+{
+	@Test
+	public void stringToFigureNameTest() {
+		assertThat(FigureName.stringToFigureName("queen")).isEqualTo(FigureName.QUEEN);
+		assertThat(FigureName.stringToFigureName("king")).isEqualTo(FigureName.KING);
+		assertThat(FigureName.stringToFigureName("rook")).isEqualTo(FigureName.ROOK);
+		assertThat(FigureName.stringToFigureName("bishop")).isEqualTo(FigureName.BISHOP);
+		assertThat(FigureName.stringToFigureName("knight")).isEqualTo(FigureName.KNIGHT);
+		assertThat(FigureName.stringToFigureName("pawn")).isEqualTo(FigureName.PAWN);
+		assertThat(FigureName.stringToFigureName("King")).isNull();
+	}
+}
