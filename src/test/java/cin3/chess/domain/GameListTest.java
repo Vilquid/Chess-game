8377b67ff303d1616f98ca794e804eedff8b4b6f
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-21 16:40:21 +0200

    Ajout TU GameList

---
+++ src/test/java/cin3/chess/domain/GameListTest.java
@@ -0,0 +1,31 @@
+package cin3.chess.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameListTest {
+	private GameList g;
+
+	@BeforeEach
+	public void before() {
+		g = new GameList();
+	}
+
+	/**
+	 * It tests that the getters and setters work
+	 */
+	@Test
+	public void gettersTest() {
+		g.setId(0L);
+		g.setWinner("winner");
+		g.setLooser("looser");
+		g.setGameId(2048L);
+
+		assertThat(g.getId()).isEqualTo(0L);
+		assertThat(g.getWinner()).isEqualTo("winner");
+		assertThat(g.getLooser()).isEqualTo("looser");
+		assertThat(g.getGameId()).isEqualTo(2048L);
+	}
+}
