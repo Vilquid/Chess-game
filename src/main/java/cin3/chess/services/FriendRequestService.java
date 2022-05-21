7191f3344bbdc8740ea114c2fcb3f2289dfb9415
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-21 14:36:21 +0200

    Service de Friend request

--- src/main/java/cin3/chess/services/FriendRequestService.java
+++ src/main/java/cin3/chess/services/FriendRequestService.java
@@ -1,4 +1,41 @@
 package cin3.chess.services;
 
-public class FriendRequestService {
+import cin3.chess.domain.FriendRequest;
+import cin3.chess.domain.User;
+import cin3.chess.repository.FriendRequestRepository;
+import cin3.chess.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FriendRequestService
+{
+	@Autowired
+	private UserRepository users;
+
+	@Autowired
+	private FriendRequestRepository friendRequests;
+
+	/**
+	 * Get all the friend requests that have been accepted by the user, and return a list of the users that sent those
+	 * requests.
+	 *
+	 * @param receiver The user who is receiving the friend request.
+	 * @return A list of users that are friends with the user that is passed in.
+	 */
+	public List<User> getFriendUserList(User receiver)
+	{
+		List<FriendRequest> friends = friendRequests.findAllByReceiverAndIsAccepted(receiver, true);
+		List<User> userFriends = new ArrayList<>();
+
+		for (FriendRequest friend: friends)
+		{
+			userFriends.add(users.findByUsername(friend.getSender()));
+		}
+
+		return userFriends;
+	}
 }
