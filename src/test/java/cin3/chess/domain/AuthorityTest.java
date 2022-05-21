96412555fd5909de3aafab975d5dafff1e871f50
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-21 15:38:48 +0200

    Ajout TU Authority

---
+++ src/test/java/cin3/chess/domain/AuthorityTest.java
@@ -0,0 +1,28 @@
+package cin3.chess.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuthorityTest {
+	private Authority auth;
+
+	@BeforeEach
+	public void before() {
+		auth = new Authority();
+	}
+
+	/**
+	 * This function tests the getters and setters of the Authority class
+	 */
+	@Test
+	public void gettersTest() {
+
+		auth.setId(2048L);
+		auth.setAuthority("username");
+
+		assertThat(auth.getId()).isEqualTo(2048L);
+		assertThat(auth.getAuthority()).isEqualTo("username");
+	}
+}
