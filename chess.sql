e04588dd792799c222baa8ef6fda43eb21c64ff8
Mathis <60876471+Vilquid@users.noreply.github.com>
2022-05-21 15:39:31 +0200

    Ajout de chess.sql
    Sert au lancement sous Linux.

---
+++ chess.sql
@@ -0,0 +1,4 @@
+-- It creates a database called chess, a role called chess, and gives the role all privileges on all tables in the public schema.
+CREATE DATABASE chess;
+CREATE ROLE chess WITH LOGIN PASSWORD 'chess';
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO chess;
